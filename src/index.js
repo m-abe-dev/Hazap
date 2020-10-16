bc39f90d5500cee0c45edd4e1421d8e741c4dd8d
masashiabe <tech.abe.jp@gmail.com>
2020-10-17 04:49:44 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { render } from "@testing-library/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
