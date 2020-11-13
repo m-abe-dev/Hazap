9d8d1c2fd6dd9449e08bb2f63282734d4dac0c6c
masashiabe <tech.abe.jp@gmail.com>
2020-11-14 04:19:10 +0900

    update eventform

--- src/features/events/eventDashboard/EventsFeed.jsx
+++ src/features/events/eventDashboard/EventsFeed.jsx
@@ -1,38 +1,35 @@
-import React from "react";
-import { Header, Segment, Feed } from "semantic-ui-react";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import {
-  getUserFeedRef,
-  firebaseObjectToArray,
-} from "../../../app/firestore/firebaseService";
-import { listenToFeed } from "../../profiles/profileActions";
-import EventFeedItem from "./EventFeedItem";
+import React from 'react';
+import { Header, Segment, Feed } from 'semantic-ui-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getUserFeedRef, firebaseObjectToArray } from '../../../app/firestore/firebaseService';
+import { listenToFeed } from '../../profiles/profileActions';
+import EventFeedItem from './EventFeedItem';
 
 export default function EventsFeed() {
   const dispatch = useDispatch();
   const { feed } = useSelector((state) => state.profile);
 
   useEffect(() => {
-    getUserFeedRef().on("value", (snapshot) => {
-      if (!snapshot.exists()) {
-        return;
-      }
-      const feed = firebaseObjectToArray(snapshot.val()).reverse();
-      dispatch(listenToFeed(feed));
-    });
+    getUserFeedRef().on('value', snapshot => {
+        if (!snapshot.exists()) {
+            return;
+        }
+        const feed = firebaseObjectToArray(snapshot.val()).reverse();
+        dispatch(listenToFeed(feed))
+    })
     return () => {
-      getUserFeedRef().off();
-    };
-  }, [dispatch]);
+        getUserFeedRef().off()
+    }
+  }, [dispatch])
 
   return (
     <>
-      <Header attached color="teal" icon="newspaper" content="News feed" />
-      <Segment attached="bottom">
+      <Header attached color='teal' icon='newspaper' content='News feed' />
+      <Segment attached='bottom'>
         <Feed>
-          {feed.map((post) => (
-            <EventFeedItem post={post} key={post.id} />
+          {feed.map(post => (
+              <EventFeedItem post={post} key={post.id} />
           ))}
         </Feed>
       </Segment>
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,58 +1,64 @@
 /* global google */
-import React, { useState } from "react";
-import { Segment, Header, Button, Confirm } from "semantic-ui-react";
-import { Link, Redirect } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { listenToSelectedEvent } from "../eventActions";
-import { Formik, Form } from "formik";
-import * as Yup from "yup";
-import MyTextInput from "../../../app/common/form/MyTextInput";
-import MyTextArea from "../../../app/common/form/MyTextArea";
-import MySelectInput from "../../../app/common/form/MySelectInput";
-import { categoryData } from "../../../app/api/categoryOptions";
-import MyDateInput from "../../../app/common/form/MyDateInput";
-import MyPlaceInput from "../../../app/common/form/MyPlaceInput";
+import React, { useState } from 'react';
+import { Segment, Header, Button, Confirm } from 'semantic-ui-react';
+import { Link, Redirect } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { listenToSelectedEvent, clearSelectedEvent } from '../eventActions';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from '../../../app/common/form/MyTextArea';
+import MySelectInput from '../../../app/common/form/MySelectInput';
+import { categoryData } from '../../../app/api/categoryOptions';
+import MyDateInput from '../../../app/common/form/MyDateInput';
+import MyPlaceInput from '../../../app/common/form/MyPlaceInput';
 import {
   listenToEventFromFirestore,
   updateEventInFirestore,
   addEventToFirestore,
   cancelEventToggle,
-} from "../../../app/firestore/firestoreService";
-import useFirestoreDoc from "../../../app/hooks/useFirestoreDoc";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
-import { toast } from "react-toastify";
+} from '../../../app/firestore/firestoreService';
+import useFirestoreDoc from '../../../app/hooks/useFirestoreDoc';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
-export default function EventForm({ match, history }) {
+export default function EventForm({ match, history, location }) {
   const dispatch = useDispatch();
   const [loadingCancel, setLoadingCancel] = useState(false);
   const [confirmOpen, setConfirmOpen] = useState(false);
   const { selectedEvent } = useSelector((state) => state.event);
   const { loading, error } = useSelector((state) => state.async);
 
+  useEffect(() => {
+    if (location.pathname !== '/createEvent') return;
+    dispatch(clearSelectedEvent());
+  }, [dispatch, location.pathname])
+
   const initialValues = selectedEvent ?? {
-    title: "",
-    category: "",
-    description: "",
+    title: '',
+    category: '',
+    description: '',
     city: {
-      address: "",
+      address: '',
       latLng: null,
     },
     venue: {
-      address: "",
+      address: '',
       latLng: null,
     },
-    date: "",
+    date: '',
   };
 
   const validationSchema = Yup.object({
-    title: Yup.string().required("You must provide a title"),
-    category: Yup.string().required("You must provide a category"),
+    title: Yup.string().required('You must provide a title'),
+    category: Yup.string().required('You must provide a category'),
     description: Yup.string().required(),
     city: Yup.object().shape({
-      address: Yup.string().required("City is required"),
+      address: Yup.string().required('City is required'),
     }),
     venue: Yup.object().shape({
-      address: Yup.string().required("Venue is required"),
+      address: Yup.string().required('Venue is required'),
     }),
     date: Yup.string().required(),
   });
@@ -70,19 +76,20 @@ export default function EventForm({ match, history }) {
   }
 
   useFirestoreDoc({
-    shouldExecute: !!match.params.id,
+    shouldExecute: match.params.id !== selectedEvent?.id && location.pathname !== '/createEvent',
     query: () => listenToEventFromFirestore(match.params.id),
     data: (event) => dispatch(listenToSelectedEvent(event)),
     deps: [match.params.id, dispatch],
   });
 
-  if (loading) return <LoadingComponent content="Loading event..." />;
+  if (loading) return <LoadingComponent content='Loading event...' />;
 
-  if (error) return <Redirect to="/error" />;
+  if (error) return <Redirect to='/error' />;
 
   return (
     <Segment clearing>
       <Formik
+        enableReinitialize
         initialValues={initialValues}
         validationSchema={validationSchema}
         onSubmit={async (values, { setSubmitting }) => {
@@ -91,7 +98,7 @@ export default function EventForm({ match, history }) {
               ? await updateEventInFirestore(values)
               : await addEventToFirestore(values);
             setSubmitting(false);
-            history.push("/events");
+            history.push('/events');
           } catch (error) {
             toast.error(error.message);
             setSubmitting(false);
@@ -99,45 +106,46 @@ export default function EventForm({ match, history }) {
         }}
       >
         {({ isSubmitting, dirty, isValid, values }) => (
-          <Form className="ui form">
-            <Header sub color="teal" content="Event Details" />
-            <MyTextInput name="title" placeholder="Event title" />
+          <Form className='ui form'>
+            <Header sub color='teal' content='Event Details' />
+            <MyTextInput name='title' placeholder='Event title' />
             <MySelectInput
-              name="category"
-              placeholder="Event category"
+              name='category'
+              placeholder='Event category'
               options={categoryData}
             />
-            <MyTextArea name="description" placeholder="Description" rows={3} />
-            <Header sub color="teal" content="Event Location Details" />
-            <MyPlaceInput name="city" placeholder="City" />
+            <MyTextArea name='description' placeholder='Description' rows={3} />
+            <Header sub color='teal' content='Event Location Details' />
+            <MyPlaceInput name='city' placeholder='City' />
             <MyPlaceInput
-              name="venue"
+              name='venue'
               disabled={!values.city.latLng}
-              placeholder="Venue"
+              placeholder='Venue'
               options={{
                 location: new google.maps.LatLng(values.city.latLng),
                 radius: 1000,
-                types: ["establishment"],
+                types: ['establishment'],
               }}
             />
             <MyDateInput
-              name="date"
-              placeholderText="Event date"
-              timeFormat="HH:mm"
+              name='date'
+              placeholderText='Event date'
+              timeFormat='HH:mm'
               showTimeSelect
-              timeCaption="time"
-              dateFormat="MMMM d, yyyy h:mm a"
+              timeCaption='time'
+              dateFormat='MMMM d, yyyy h:mm a'
+              autoComplete='off'
             />
             {selectedEvent && (
               <Button
                 loading={loadingCancel}
-                type="button"
-                floated="left"
-                color={selectedEvent.isCancelled ? "green" : "red"}
+                type='button'
+                floated='left'
+                color={selectedEvent.isCancelled ? 'green' : 'red'}
                 content={
                   selectedEvent.isCancelled
-                    ? "Reactivate event"
-                    : "Cancel Event"
+                    ? 'Reactivate event'
+                    : 'Cancel Event'
                 }
                 onClick={() => setConfirmOpen(true)}
               />
@@ -145,18 +153,18 @@ export default function EventForm({ match, history }) {
             <Button
               loading={isSubmitting}
               disabled={!isValid || !dirty || isSubmitting}
-              type="submit"
-              floated="right"
+              type='submit'
+              floated='right'
               positive
-              content="Submit"
+              content='Submit'
             />
             <Button
               disabled={isSubmitting}
               as={Link}
-              to="/events"
-              type="submit"
-              floated="right"
-              content="Cancel"
+              to='/events'
+              type='submit'
+              floated='right'
+              content='Cancel'
             />
           </Form>
         )}
@@ -164,8 +172,8 @@ export default function EventForm({ match, history }) {
       <Confirm
         content={
           selectedEvent?.isCancelled
-            ? "This will reactivate the event - are you sure?"
-            : "This will cancel the event - are you sure?"
+            ? 'This will reactivate the event - are you sure?'
+            : 'This will cancel the event - are you sure?'
         }
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
